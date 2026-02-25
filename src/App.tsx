c2d74c70d0f0f90152b46bc5ab5814734c4a0b3a
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-02-25 16:17:32 -0300

    feat: add React Router DOM router context

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/theme';
+import { Router } from './Router';
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-      <h1>Hello world!</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
