e0ed181d6e1d3334ef154357f0b974860ce9525e
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2026-03-30 12:21:33 -0300

    fix: add limit to post title

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,14 +1,28 @@
+import { formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { PostContainer, PostTitle } from "./styles";
 
-export function Post(){
+interface PostProps {
+  id: number;
+  title: string;
+  createdAt: string;
+  content: string;
+}
+
+export function Post({id, title, createdAt, content}: PostProps){
+  const formattedDate = formatDistanceToNow(createdAt, {
+    locale: ptBR,
+    addSuffix: true, 
+  }).replace('cerca de', '').replace('h', 'H');
+  
   return (
-    <PostContainer>
+    <PostContainer to={`/posts/${id}`}>
       <PostTitle>
-        <h3>JavaScript data types and data structures</h3>
-        <span>Há 1 dia</span>
+        <h3 title={title}>{title}</h3>
+        <span title={createdAt}>{formattedDate}</span>
       </PostTitle>
       <p>
-        Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.
+        {content}
       </p>
     </PostContainer>
   )
--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,11 +1,14 @@
 import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import Markdown from 'react-markdown';
 
-export const PostContainer = styled.div`
+export const PostContainer = styled(Link)`
   height: 26rem;
   padding: 3.2rem;
   display: flex;
   flex-direction: column;
   background-color: ${({theme})=> theme["base-post"]};
+  text-decoration: none;
 
   border-radius: 10px;
 
@@ -17,17 +20,28 @@ export const PostContainer = styled.div`
     -webkit-box-orient: vertical;
     overflow: hidden;
   }
+
+  transition: transform 0.2s ease-out;
+
+  &:hover {
+    transform: scale(1.02)
+  }
 `;
 
 export const PostTitle = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 2rem;
+  gap: 0.8rem;
 
   > h3 { 
     max-width: 28.4rem;
     font: ${({theme})=> theme["font-title-m"]};
     color: ${({theme}) => theme["base-title"]};
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
   }
 
   > span {
@@ -35,3 +49,7 @@ export const PostTitle = styled.div`
     font: ${({theme})=> theme["font-text-m"]};
   }
 `;
+
+export const PostContent = styled(Markdown)`
+
+`
